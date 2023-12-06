a239c415fbe00283fe14f88a825637c14073d166
Uttam Nagvadiya <uttam.git835@gmail.com>
2023-12-06 21:51:13 +0530

    .

--- Preparation/Programs/Pattern.java
+++ Preparation/Programs/Pattern.java
@@ -20,17 +20,16 @@ public class Pattern {
 
         int count = 1;
 
-        for (int i=0; i<rows; i++){
-            if (i%2==0){
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0) {
                 for (int j = 0; j < rows; j++) {
-                    System.out.print(count+ "\t");
+                    System.out.print(count + "\t");
                     count++;
                 }
-            }
-            else{
-                int temp = count+rows-1;
-                for (int k = rows-1; k >= 0; k--) {
-                    System.out.print(temp+ "\t");
+            } else {
+                int temp = count + rows - 1;
+                for (int k = rows - 1; k >= 0; k--) {
+                    System.out.print(temp + "\t");
                     temp--;
                 }
                 count += rows;
