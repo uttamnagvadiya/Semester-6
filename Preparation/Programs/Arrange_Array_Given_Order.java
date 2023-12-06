dad56065947fc9e12ff9bba1041e15a86e0b8aff
Uttam Nagvadiya <uttam.git835@gmail.com>
2023-12-06 21:49:56 +0530

    .

--- Preparation/Programs/Arrange_Array_Given_Order.java
+++ Preparation/Programs/Arrange_Array_Given_Order.java
@@ -5,9 +5,24 @@ to its right, third minimum to its left, fourth minimum to its right and so on.
  Input: [9, 5, 7, 1, 8, 3, 6, 2, 4, 10]     Output: [9, 7, 5, 3, 1, 2, 4, 6, 8, 10]
 */
 
+import java.util.Arrays;
 
 public class Arrange_Array_Given_Order {
     public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int result[] = new int[arr.length];
+        Arrays.sort(arr);
+        int mid = (arr.length / 2) - 1;
 
+        result[mid] = arr[0];
+        int i = 1;
+        for (int j = 1; i + 1 < arr.length; i += 2, j++) {
+            result[mid + j] = arr[i];
+            result[mid - j] = arr[i + 1];
+        }
+        if (i == arr.length - 1)
+            result[arr.length - 1] = arr[arr.length - 1];
+
+        System.out.println(Arrays.toString(result));
     }
 }
--- Preparation/Programs/Pattern.java
+++ Preparation/Programs/Pattern.java
@@ -5,12 +5,37 @@ Write a program to print following pattern
 11 12 13 14 15
 20 19 18 17 16
 21 22 23 24 25
- |
-(%2 == 0) ? -- : ++
+ 
+(row(i)%2 == 0) ? -- : ++
 */
+
+import java.util.Scanner;
 
 public class Pattern {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int rows = sc.nextInt();
+
+        int count = 1;
+
+        for (int i=0; i<rows; i++){
+            if (i%2==0){
+                for (int j = 0; j < rows; j++) {
+                    System.out.print(count+ "\t");
+                    count++;
+                }
+            }
+            else{
+                int temp = count+rows-1;
+                for (int k = rows-1; k >= 0; k--) {
+                    System.out.print(temp+ "\t");
+                    temp--;
+                }
+                count += rows;
+            }
+            System.out.println();
+        }
     }
 }
